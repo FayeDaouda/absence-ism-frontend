c00033228ef9458c9d2a2eac445dd6b5db5ec271
Daouda FAYE <daoudafaye2017@gmail.com>
2025-06-16 15:49:11 +0000

    sas

--- src/app/admin/layout/admin-layout/admin-layout.component.ts
+++ src/app/admin/layout/admin-layout/admin-layout.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common'; // ✅ à ajouter
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule], // ✅ ajoute CommonModule
+  imports: [CommonModule, RouterModule],
   templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.css']
 })
-export class AdminLayoutComponent {
+export class AdminLayoutComponent implements OnInit {
+
+  utilisateur: any = null;
+
+  ngOnInit(): void {
+    const utilisateurString = localStorage.getItem('utilisateur');
+    if (utilisateurString) {
+      this.utilisateur = JSON.parse(utilisateurString);
+    }
+  }
+
   logout(): void {
     localStorage.clear();
     window.location.href = '/login';
